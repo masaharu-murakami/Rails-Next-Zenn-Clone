0d257a2a5bacfa9fb6b69b0e2ce19d09d2eb9b64
MasaharuMurakami <89374344+masaharu-murakami@users.noreply.github.com>
2024-10-28 23:54:57 +0900

    add :rubocop

--- rails/app/controllers/api/v1/health_check_controller.rb
+++ rails/app/controllers/api/v1/health_check_controller.rb
@@ -1,6 +1,12 @@
-class Api::V1::HealthCheckController < ApplicationController
-  def index
-    # binding.pry
-    render json: { message: "Success Health Check!" }, status: :ok
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class HealthCheckController < ApplicationController
+      def index
+        # binding.pry
+        render json: { message: 'Success Health Check!' }, status: :ok
+      end
+    end
   end
 end
